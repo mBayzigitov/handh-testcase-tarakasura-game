5aa4bf76920d827571582f8e394d7189c3482493
Murat Bayzigitov <bayzigitov.murat@gmail.com>
2023-09-26 15:55:12 +0300

    fix: Ascii arts are fixed

--- src/main/java/ru/handh/Main.java
+++ src/main/java/ru/handh/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static final Scanner scanner = new Scanner(System.in);
     public static Player current_hero;
     public static void main(String[] args) throws InterruptedException {
-        
+
         ////////////////// MAIN LOGIC OF THE GAME ///////////////////
         printLogo();
 
